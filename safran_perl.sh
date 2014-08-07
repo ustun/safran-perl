6f6efc78f378a12e58aa0e6b1bc6ce2db75a8c4c
Ustun Ozgur <ustunozgur@gmail.com>
2014-08-07 22:41:28 +0300

    Golf

--- safran_perl.sh
+++ safran_perl.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 curl -sL safran.io/feed.rss | perl -wnl -e '
-BEGIN{my $counter = 0;}
-/<title/ and $counter++;
-/<title/ and s/\s*<\/?title>//g and printf("%2d. %s\n\n", $counter, $_);
-/<link/ and s/\s*<\/?link>//g and print "    $_\n";
-/<description/ and s/\s*<\/?description>//g and print "    $_\n";'
+s/\s*<\/?title>//g and ++$counter and printf("%2d. %s\n\n", $counter, $_);
+s/\s*<\/?(link|description)>//g and print "    $_\n";'
